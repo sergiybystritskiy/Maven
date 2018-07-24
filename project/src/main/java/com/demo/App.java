f6952de29ca9828f631277682f698dc3d4cc965b
sergii_bystritskyi <sergii_bystritskyi@epam.com>
2018-07-24 14:53:24 +0300

    Log and binary files removed, formatting added

--- project/src/main/java/com/demo/App.java
+++ project/src/main/java/com/demo/App.java
@@ -3,15 +3,13 @@ package com.demo;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 
-public class App 
-{
+public class App {
     final static Logger logger = LogManager.getLogger(App.class);
-	
-    public static void main( String[] args )
-    {
-	logger.info("This is info");
-	logger.warn("This is warn");
-	logger.error("This is error");
-	logger.fatal("This is fatal");
+
+    public static void main(String[] args) {
+        logger.info("This is info");
+        logger.warn("This is warn");
+        logger.error("This is error");
+        logger.fatal("This is fatal");
     }
 }
